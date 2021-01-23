f50ffd4b476cc6b4e7e809bf233206b1c6f5f098
mrcatcis <domozhirov.anton2015@ya.ru>
2021-01-23 21:46:11 +0300

    done all

--- 5.3.py
+++ 5.3.py
@@ -1,18 +1,23 @@
-from PyQt5.QtWidgets import QWidget, QApplication
+from PyQt5.QtWidgets import QWidget, QApplication, QMessageBox
 from PyQt5.QtGui import QPainter, QColor, QPolygon
 from PyQt5.QtCore import Qt, QPoint
 from math import pi, sin, cos
 
-
+# Основной класс с окном
 class Window(QWidget):
+
+    # Класс правильного многоугольника
     class Polygon():
-        def __init__(self, n, pos, size):
+        
+        # Инициализация класса
+        def __init__(self, n: int, pos: QPoint, size: int):
             self.n = n
             self.pos = pos
             self.size = size
             self.polygon = self.createPolygon(n, pos, size)
 
-        def createPolygon(self, n: int, pos: QPoint, size: int):
+        # Создание многоугольника
+        def createPolygon(self, n: int, pos: QPoint, size: int) -> QPolygon:
             polygon = []
             delt = 2 * pi / n
             for i in range(n):
@@ -21,32 +26,51 @@ class Window(QWidget):
                                       pos.y() + int(cos(i) * size)))
             return QPolygon(polygon)
 
+        # Добавление угла в многоугольник
         def addAngle(self):
             self.n += 1
             self.polygon = self.createPolygon(self.n, self.pos, self.size)
 
+    # Инициализация главного окна
     def __init__(self):
         super().__init__()
-        self.setGeometry(400, 200, 800, 600)
-        self.setWindowTitle('5.2')
+        self.setWindowTitle('5.3')
         self.show()
-
         self.polygons = []
 
-    def deletePolygons(self):
-        self.polygons.clear()
+    # Возврашает количество многоугольников
+    def countPolygons(self) -> int:
+        return len(self.polygons)
 
+    # Создаёт и показывает окно об ошибке
+    def showError(self):
+        errorDialog = QMessageBox()
+        errorDialog.setWindowTitle('5.3')
+        errorDialog.setText('Отсутствуют фигуры на поле')
+        errorDialog.setIcon(QMessageBox.Warning)
+        errorDialog.exec_()
+
+    # Удаление всех многоугольников с поля
+    def deletePolygons(self):
+        if self.countPolygons() > 0:
+            self.polygons.clear()
+        else:
+            self.showError()
+
+    # Обработка события нажатия клавиши
     def keyPressEvent(self, event):
         if event.key() == Qt.Key_Delete:
             self.deletePolygons()
             self.update()
 
-    def findPolygon(self, pos):
+    # Нахождение многоугольника в котором лежит заданная точка, возвращает -1 если не найдено
+    def findPolygon(self, pos: QPoint) -> int:
         for i in range(len(self.polygons)):
             if self.polygons[i].polygon.containsPoint(pos, Qt.OddEvenFill):
                 return i
         return -1
 
+    # Обработка события нажатия кнопки мыши
     def mousePressEvent(self, event):
         x, y = event.pos().x(), event.pos().y()
         index = self.findPolygon(QPoint(x, y))
@@ -54,14 +78,17 @@ class Window(QWidget):
             self.polygons.append(self.Polygon(3, QPoint(x, y), 50))
         else:
             self.polygons[index].addAngle()
+
         self.update()
 
+    # Обработка события рисования
     def paintEvent(self, event):
         self.painter = QPainter(self)
         self.painter.begin(self)
         self.drawPolygons()
         self.painter.end()
 
+    # Отрисовка всех многоугольников
     def drawPolygons(self):
         for polygon in self.polygons:
             self.painter.drawPolygon(polygon.polygon)
